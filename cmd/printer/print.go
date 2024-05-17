8c884f401cb4f9f946e058ba57f3fd84d1fb7884
Leonardo Campitelli <leonardo932.campitelli@gmail.com>
2024-05-17 17:03:08 +0200

    add printer package

--- cmd/printer/print.go
+++ cmd/printer/print.go
@@ -4,6 +4,8 @@ import "github.com/fatih/color"
 
 type Printer interface {
 	Info(message string)
+	ConfigMainKey(message string)
+	ConfigSubKey(message string)
 }
 
 type printer struct{}
@@ -14,4 +16,12 @@ func NewPrinter() Printer {
 
 func (p *printer) Info(message string) {
 	color.Cyan(message)
+}
+
+func (p *printer) ConfigMainKey(message string) {
+	color.New(color.FgWhite).Add(color.Bold).Add(color.BgBlack).Println(message)
+}
+
+func (p *printer) ConfigSubKey(message string) {
+	color.New(color.FgCyan).Add(color.Bold).Println(message)
 }
